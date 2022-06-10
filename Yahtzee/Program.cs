4cd65b95c593f0dc44254c820c6598cf882ee4b8
Kim Mann <kimmania@gmail.com>
2022-06-10 15:09:27 -0400

    Read games from file

--- Yahtzee/Program.cs
+++ Yahtzee/Program.cs
@@ -1,4 +1,5 @@
-﻿using Yahtzee.Models;
+﻿using Yahtzee;
+using Yahtzee.Models;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
@@ -6,3 +7,12 @@ Console.WriteLine("Hello, World!");
 //will use to generate some randome files
 //Game game = new Game();
 //Console.Write(game.ToString());
+
+
+//read data in from file
+var games = StaticUtilities.LoadGamesFromFile("./SampleFiles/simpleTest.txt");
+foreach (var game in games)
+{
+    Console.WriteLine($"{game}\n\n");
+}
+
---
+++ Yahtzee/StaticUtilities.cs
@@ -0,0 +1,21 @@
+﻿using Yahtzee.Models;
+
+namespace Yahtzee
+{
+    internal class StaticUtilities
+    {
+        public static IEnumerable<Game> LoadGamesFromFile(string fileName, int setLen = 13)
+        {
+            using var reader = new StreamReader(fileName);
+            while (!reader.EndOfStream)
+            {
+                var set = new List<string>();
+                for (var i = 0; i < setLen && !reader.EndOfStream; i++)
+                {
+                    set.Add(reader.ReadLine());
+                }
+                yield return new Game(set);
+            }
+        }
+    }
+}
