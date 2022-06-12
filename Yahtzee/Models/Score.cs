3810a0721f166564c8e631f72ea8b53e20a4927b
Kim Mann <kimmania@gmail.com>
2022-06-12 15:29:57 -0400

    Update Score.cs

--- Yahtzee/Models/Score.cs
+++ Yahtzee/Models/Score.cs
@@ -24,6 +24,27 @@ namespace Yahtzee.Models
         public Score() { } //this will be used to create the final scoring
 
         /// <summary>
+        /// Copy an existing score that has already been calculated
+        /// </summary>
+        /// <param name="scoreToCopy"></param>
+        public Score(Score scoreToCopy) 
+        {
+            this.Ones = scoreToCopy.Ones;
+            this.Twos = scoreToCopy.Twos;
+            this.Threes = scoreToCopy.Threes;
+            this.Fours = scoreToCopy.Fours;
+            this.Fives = scoreToCopy.Fives;
+            this.Sixes = scoreToCopy.Sixes;
+            this.Chance = scoreToCopy.Chance;
+            this.ThreeOfKind = scoreToCopy.ThreeOfKind;
+            this.FourOfKind = scoreToCopy.FourOfKind;
+            this.FiveOfKind = scoreToCopy.FiveOfKind;
+            this.ShortStraight = scoreToCopy.ShortStraight;
+            this.LongStraight = scoreToCopy.LongStraight;
+            this.FullHouse = scoreToCopy.FullHouse;
+        }
+
+        /// <summary>
         /// Evaluate a single throw's possible scores
         /// </summary>
         /// <param name="dice"></param>
@@ -32,57 +53,31 @@ namespace Yahtzee.Models
             //going to evaluate a single throw
             int sumOfAllDice = 0; // used for chance, 3 of kind, 4 of kind -- calculating once
             //counting, not summing
-            int ones = 0;
-            int twos = 0;
-            int threes = 0;
-            int fours = 0;
-            int fives = 0;
-            int sixes = 0;
+            int[] counts = new int[7]; // ignoring the zero entry
 
             //loop over the dice and track values that we have
             foreach (var die in dice)
             {
                 sumOfAllDice += die;
-                switch (die)
-                {
-                    case 1:
-                        ones++;
-                        break;
-                    case 2:
-                        twos++;
-                        break;
-                    case 3:
-                        threes++;
-                        break;
-                    case 4:
-                        fours++;
-                        break;
-                    case 5:
-                        fives++;
-                        break;
-                    case 6:
-                        sixes++;
-                        break;
-                }
+                counts[die]++;
             }
 
             Chance = sumOfAllDice;
-
-            Ones = ones;
-            Twos = twos * 2;
-            Threes = threes * 3;
-            Fours = fours * 4;
-            Fives = fives * 5;
-            Sixes = sixes * 6;
+            Ones = counts[1];
+            Twos = counts[2] * 2;
+            Threes = counts[3] * 3;
+            Fours = counts[4] * 4;
+            Fives = counts[5] * 5;
+            Sixes = counts[6] * 6;
 
             //check for 5 of a kind, that automatically means we fill in 4 and 3 of kind
             if (
-                ones == 5 ||
-                twos == 5 ||
-                threes == 5 ||
-                fours == 5 ||
-                fives == 5 ||
-                sixes == 5
+                counts[1] == 5 ||
+                counts[2] == 5 ||
+                counts[3] == 5 ||
+                counts[4] == 5 ||
+                counts[5] == 5 ||
+                counts[6] == 5
             )
             {
                 FiveOfKind = 50;
@@ -91,12 +86,12 @@ namespace Yahtzee.Models
             }
             //not 5 of a kind, so check for 4 of kind, automatically 3 of a kind 
             else if (
-                ones > 3 ||
-                twos > 3 ||
-                threes > 3 ||
-                fours > 3 ||
-                fives > 3 ||
-                sixes > 3
+                counts[1] > 3 ||
+                counts[2] > 3 ||
+                counts[3] > 3 ||
+                counts[4] > 3 ||
+                counts[5] > 3 ||
+                counts[6] > 3
             )
             {
                 FourOfKind = sumOfAllDice;
@@ -104,23 +99,23 @@ namespace Yahtzee.Models
             }
             //not 4 of a kind, so evaluate for three of a kind
             else if (
-                ones > 2 ||
-                twos > 2 ||
-                threes > 2 ||
-                fours > 2 ||
-                fives > 2 ||
-                sixes > 2
+                counts[1] > 2 ||
+                counts[2] > 2 ||
+                counts[3] > 2 ||
+                counts[4] > 2 ||
+                counts[5] > 2 ||
+                counts[6] > 2
             )
             {
                 ThreeOfKind = sumOfAllDice;
 
                 if (
-                    ones == 2 ||
-                    twos == 2 ||
-                    threes == 2 ||
-                    fours == 2 ||
-                    fives == 2 ||
-                    sixes == 2
+                    counts[1] == 2 ||
+                    counts[2] == 2 ||
+                    counts[3] == 2 ||
+                    counts[4] == 2 ||
+                    counts[5] == 2 ||
+                    counts[6] == 2
                 )
                     //checking for full house
                     FullHouse = 40;
@@ -132,19 +127,19 @@ namespace Yahtzee.Models
                 //check for long straight, and if not matching, then check for short
                 if (
                     (
-                        ones == 1 &&
-                        twos == 1 &&
-                        threes == 1 &&
-                        fours == 1 &&
-                        fives == 1
+                        counts[1] == 1 &&
+                        counts[2] == 1 &&
+                        counts[3] == 1 &&
+                        counts[4] == 1 &&
+                        counts[5] == 1
                     )
                     ||
                     (
-                        twos == 1 &&
-                        threes == 1 &&
-                        fours == 1 &&
-                        fives == 1 &&
-                        sixes == 1
+                        counts[2] == 1 &&
+                        counts[3] == 1 &&
+                        counts[4] == 1 &&
+                        counts[5] == 1 &&
+                        counts[6] == 1
                     )
                 )
                 {
@@ -152,11 +147,11 @@ namespace Yahtzee.Models
                     LongStraight = 35;
                 } else if //not a long straight so now checking for small straight
                 (
-                    (ones > 0 && twos > 0 && threes > 0 && fours > 0)
+                    (counts[1] > 0 && counts[2] > 0 && counts[3] > 0 && counts[4] > 0)
                     ||
-                    (twos > 0 && threes > 0 && fours > 0 && fives > 0)
+                    (counts[2] > 0 && counts[3] > 0 && counts[4] > 0 && counts[5] > 0)
                     ||
-                    (threes > 0 && fours > 0 && fives > 0 && sixes > 0)
+                    (counts[3] > 0 && counts[4] > 0 && counts[5] > 0 && counts[6] > 0)
                 )
                     ShortStraight = 25;
             }
@@ -165,8 +160,5 @@ namespace Yahtzee.Models
         public override string ToString()
             => $"{Ones} {Twos} {Threes} {Fours} {Fives} {Sixes} {Chance} {ThreeOfKind} {FourOfKind} {FiveOfKind} {ShortStraight} {LongStraight} {FullHouse} {Bonus} {TotalScore}";
 
-        public string PrintPossibleScores()
-            =>$"{Ones.ToString("00")} {Twos.ToString("00")} {Threes.ToString("00")} {Fours.ToString("00")} {Fives.ToString("00")} {Sixes.ToString("00")} {Chance.ToString("00")} {ThreeOfKind.ToString("00")} {FourOfKind.ToString("00")} {FiveOfKind.ToString("00")} {ShortStraight.ToString("00")} {LongStraight.ToString("00")} {FullHouse.ToString("00")}";
-        
     }
 }
