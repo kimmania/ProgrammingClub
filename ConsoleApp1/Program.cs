311e4bd5c615cae4cfaaedcc5aada60f7ca5ab7c
Kim Mann <kimmania@gmail.com>
2022-06-18 12:24:55 -0400

    Working version of the code, success against sample data

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WheresWaldorf;
+
+var cases = StaticUtilities.LoadCasesFromFile("./SampleInput.txt");
+foreach (var instance in cases)
+{
+    Console.WriteLine(instance.SolveCase());
+}
