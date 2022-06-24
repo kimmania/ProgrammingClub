ce7065aaad1619e2f828204d5645f29efa3b1789
Kim Mann <kimmania@gmail.com>
2022-06-24 16:26:59 -0400

    Solve common permutation

---
+++ CommonPermutation/StaticUtilities.cs
@@ -0,0 +1,16 @@
+﻿namespace CommonPermutation
+{
+    internal class StaticUtilities
+    {
+        public static IEnumerable<WordPair> LoadContentFromFile(string fileName)
+        {
+            using var reader = new StreamReader(fileName);
+            while (!reader.EndOfStream)
+            {
+                string a = reader.ReadLine();
+                string b = reader.ReadLine();
+                yield return new WordPair(a, b);
+            }
+        }
+    }
+}
