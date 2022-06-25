b9adee2ba48239402024d060db5ce4060e0040ba
Kim Mann <kimmania@gmail.com>
2022-06-25 11:15:05 -0400

    Initial setup to read sample file

---
+++ CryptKicker2/EncryptedMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace CryptKicker2
+{
+    internal class EncryptedMessage
+    {
+        private readonly List<string> Data;
+        public EncryptedMessage(List<string> data) => this.Data = data;
+
+        public string Decrypt()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ CryptKicker2/Program.cs
@@ -0,0 +1,7 @@
+﻿using CryptKicker2;
+
+var messages = StaticUtilities.LoadCasesFromFile("./InputFiles/ProblemSampleInput.txt");
+foreach (var message in messages)
+{
+    Console.WriteLine(message.Decrypt());
+}
