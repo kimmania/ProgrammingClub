dc938b6feb5ad81fdd6d1845baad52aae261c8c0
Kim Mann <kimmania@gmail.com>
2022-07-09 08:32:29 -0400

    Update Program.cs

--- FileFragmentation/Program.cs
+++ FileFragmentation/Program.cs
@@ -8,3 +8,10 @@ foreach (var set in sets)
     Console.WriteLine();
 }
 
+//basic outline:
+//  1. Group by length of words
+//  2. Record smallest length and longest length
+//  3. Pair these up to figure out possible solutions
+//  4. Loop through other pairings by looking at the next highest from the low starting and the next lowest from the high end
+//  5. add to counts of the possible solutions if they match the determined possible solutions
+//  6. Take the value with the highest number of matching solution as the answer, or the first if there isn't a highest number
