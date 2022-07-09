a1cfc106a0cfd5389ff5ed9040076b8a21387670
Kim Mann <kimmania@gmail.com>
2022-07-09 10:04:07 -0400

    Update DataSet.cs

--- FileFragmentation/DataSet.cs
+++ FileFragmentation/DataSet.cs
@@ -5,20 +5,19 @@ namespace FileFragmentation
     {
         private readonly List<string> Data;
         private Dictionary<string, int> possibleResults = new Dictionary<string, int>();
-
+        //setting the lengths so that we can rank appropriately based on data
+        private int minLength { get; set; } = 512;
+        private int maxLength { get; set; } = 1;
         public DataSet(List<string> data) => Data = data;
 
         public string Defrag()
         {
-            var groupedBySize = Data.ToLookup(x => x.Length);
-            var numberOfPairings = Data.Count / 2;
-            
-            //special case
-            if (numberOfPairings == 1)
+            //special case -- test for it before parsing any of the data further
+            if (Data.Count / 2 == 1)
                 return (string.Join("", Data));
-
-            var minLength = groupedBySize.Min(x => x.Key);
-            var maxLength = groupedBySize.Max(x => x.Key);
+            
+            //was going to use a ToLookup, but I need to interact with the contents as a list instead of an IEnumerable
+            Dictionary<int, List<string>> groupedBySize = BuildDataLengthDictionary();
 
             //add all combinations for the shortest and the longest
             foreach (var min in groupedBySize[minLength])
@@ -37,38 +36,64 @@ namespace FileFragmentation
             //so really, below, we just need to track the new ones matching the existing sets
             for (int groupingIndex = ++minLength; groupingIndex <= --maxLength; groupingIndex++)
             {
-                if (groupedBySize[groupingIndex] == null)
+                if (groupedBySize.ContainsKey(groupingIndex) == false)
                     //this presumed bucket doesn't exist, so move on to the next pairing to evaluate
                     continue;
 
-                var lowerValues = groupedBySize[groupingIndex].ToList();
-                var upperValues = groupedBySize[maxLength].ToList();
+                //pointers to the arrays we are going to walk through
+                List<string> lowerValues = groupedBySize[groupingIndex];
+                List<string> upperValues = groupedBySize[maxLength];
 
                 //looping over the groups
                 for (int lowerValueIndex = 0; lowerValueIndex < lowerValues.Count; lowerValueIndex++)
                 {
                     for (int upperValueIndex = 0; upperValueIndex < upperValues.Count; upperValueIndex++)
                     {
-                        var min = lowerValues[lowerValueIndex];
-                        var max = upperValues[upperValueIndex];
                         if (groupingIndex == maxLength && lowerValueIndex == upperValueIndex)
                         {
-                            //skip adding itself to itself into the dictionary (skews the numbers)
+                            //in the situation where the middle ends up being the same from both directions
+                            //we want to skip evaluating adding a record to itself
                             continue;
                         }
+
+                        var min = lowerValues[lowerValueIndex];
+                        var max = upperValues[upperValueIndex];
 
                         var first = Match(min, max);
                         if (possibleResults.ContainsKey(first))
                             possibleResults[first]++;
 
                         var second = Match(max, min);
-                        if (first != second &&possibleResults.ContainsKey(second))
-                            possibleResults[second]++;   
+                        if (first != second && possibleResults.ContainsKey(second))
+                            possibleResults[second]++;
                     }
                 }
             }
 
             return possibleResults.OrderByDescending(x => x.Value).First().Key;
+        }
+
+        private Dictionary<int, List<string>> BuildDataLengthDictionary()
+        {
+            Dictionary<int, List<string>> groupedBySize = new Dictionary<int, List<string>>();
+            foreach (var item in Data)
+            {
+                if (groupedBySize.ContainsKey(item.Length))
+                    groupedBySize[item.Length].Add(item);
+                else
+                {
+                    groupedBySize.Add(item.Length, new List<string> { item });
+                    
+                    //update the lengths based on not having seen before
+                    if (minLength > item.Length)
+                        minLength = item.Length;
+
+                    if (maxLength < item.Length)
+                        maxLength = item.Length;
+                }
+            }
+
+            return groupedBySize;
         }
 
         private void AddToPossibleSolutions(string min, string max)
